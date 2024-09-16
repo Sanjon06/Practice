735fe221429e61766f8e22122792dac7f95454dc
Sanjay <bkss.sinha@gmail.com>
2024-09-16 11:13:22 +0530

    Upload Kadane's

--- untitled/src/kadane_algo.java
+++ untitled/src/kadane_algo.java
@@ -1,3 +1,25 @@
-public class kadane_algo {
+class GfG {
 
+    // Function to find the maximum subarray sum
+    static int maxSubarraySum(int[] arr) {
+        int res = arr[0];
+        int maxEnding = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            
+            // Find the maximum sum ending at index i by either extending 
+            // the maximum sum subarray ending at index i - 1 or by
+            // starting a new subarray from index i
+            maxEnding = Math.max(maxEnding + arr[i], arr[i]);
+          
+            // Update res if maximum subarray sum ending at index i > res
+            res = Math.max(res, maxEnding);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 3, -8, 7, -1, 2, 3};
+        System.out.println(maxSubarraySum(arr));
+    }
 }
